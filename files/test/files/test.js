b2c5125799295ff8325630b963890ab2e9bc7f67
Florian Schroedl Home <flo.schroedl@gmail.com>
2017-01-11 17:14:33 +0100

    Add export to test js

--- files/test/files/test.js
+++ files/test/files/test.js
@@ -1,8 +1,12 @@
 var lorem = "lorem"
 var loremIpsum = lorem + 'ipsum';
 
-function myFunction(args) {
+export default function myFunction(args) {
+
+
   var div = document.querySelector('div');
   var myUglyArr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'    ]
   var myUglyObj = { one: 'one', 'two': 'two', rest: myUglyArr}
+
+
 }
