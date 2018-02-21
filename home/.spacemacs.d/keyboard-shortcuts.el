8bcf124154ee1cd3e05b44e3f9862bcedef93ff1
Florian Schroedl <hello@florianschroedl.com>
2018-02-21 17:29:41 +0100

    Add buffer switching normal mode bindings like in vim
    * home/.spacemacs.d/keyboard-shortcuts.el:

--- home/.spacemacs.d/keyboard-shortcuts.el
+++ home/.spacemacs.d/keyboard-shortcuts.el
@@ -1,2 +1,7 @@
 (define-key company-active-map (kbd "C-w") 'evil-delete-backward-word)
 (define-key helm-map (kbd "C-w") 'evil-delete-backward-word)
+
+;; Buffer switching
+(define-key evil-normal-state-map (kbd "gb") 'evil-switch-to-windows-last-buffer)
+(define-key evil-normal-state-map (kbd "gn") 'next-buffer)
+(define-key evil-normal-state-map (kbd "gp") 'previous-buffer)
