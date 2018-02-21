078e15b025219535bd4114ae6573566c0033f355
Florian Schroedl <hello@florianschroedl.com>
2018-02-21 09:55:14 +0100

    Enable persistent undo-tree
    * home/.spacemacs.d/gui-interactions.el:

--- home/.spacemacs.d/gui-interactions.el
+++ home/.spacemacs.d/gui-interactions.el
@@ -1,2 +1,7 @@
 ;; Fix the colors for osx
 (setq ns-use-srgb-colorspace nil)
+;; Store undos after closing emacs
+;; enabling to undo after closing a file
+(global-undo-tree-mode)
+(setq undo-tree-auto-save-history t)
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
