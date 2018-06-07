788ee5a3731802788c286c1c4a2d77edd3760fe2
Florian Schroedl <hello@florianschroedl.com>
2018-06-07 10:49:33 +0200

    Fix gt bindings

--- home/.spacemacs.d/layers/floscr-git/keybindings.el
+++ home/.spacemacs.d/layers/floscr-git/keybindings.el
@@ -1,4 +1,2 @@
-(spacemacs/set-leader-keys
-  "gt" 'floscr-git/stage-and-commit-current-buffer
-  "gT" 'time-machine-transient-state
-  )
+(spacemacs/set-leader-keys "gt" 'floscr-git/magit-stage-and-commit-current-buffer)
+(spacemacs/set-leader-keys "gT" 'time-machine-transient-state)
