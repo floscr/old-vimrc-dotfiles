bd2de131b550fe70416cc277baff68fce5fe453e
Florian Schroedl <hello@florianschroedl.com>
2018-06-08 08:59:41 +0200

    Add binding to save all org buffers

--- home/.spacemacs.d/layers/floscr-org/keybindings.el
+++ home/.spacemacs.d/layers/floscr-org/keybindings.el
@@ -1,8 +1,13 @@
+;; Annotate
 (spacemacs/set-leader-keys "aof" 'org-annotate-file)
 
+;; Rifle
 (spacemacs/set-leader-keys "hr" 'nil)
 (spacemacs/set-leader-keys "hrr" 'helm-org-rifle)
 (spacemacs/set-leader-keys "hrc" 'helm-org-rifle-current-buffer)
 (spacemacs/set-leader-keys "hrd" 'helmrorg-rifle-directories)
 (spacemacs/set-leader-keys "hrf" 'helm-org-rifle-files)
 ;; (define-key org-mode (kbd ", s d") 'org-cut-subtree)
+
+;; General
+(spacemacs/set-leader-keys "aoS" 'org-save-all-org-buffers)
