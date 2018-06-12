f88bb10594260ee04b7bde1a106f102da106d931
Florian Schroedl <hello@florianschroedl.com>
2018-06-12 09:14:58 +0200

    Preview File with TAB on Neotree

--- home/.spacemacs.d/layers/floscr-defaults/packages.el
+++ home/.spacemacs.d/layers/floscr-defaults/packages.el
@@ -1,2 +1,10 @@
 (defconst floscr-defaults-packages
-  '())
+  '(neotree))
+
+(defun floscr-defaults/post-init-neotree ()
+  (use-package neotree
+    :config
+    (evilified-state-evilify-map neotree-mode-map
+      :mode neotree-mode
+      :bindings
+      (kbd "TAB") 'neotree-quick-look)))
