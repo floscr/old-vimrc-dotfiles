e60a1bb64e049f4a1c40426952f405db93c3b44f
Florian Schroedl <hello@florianschroedl.com>
2018-06-26 23:08:53 +0200

    Cleanup tabbar style

--- home/.spacemacs.d/layers/tabbar/packages.el
+++ home/.spacemacs.d/layers/tabbar/packages.el
@@ -26,62 +26,62 @@ which require an initialization must be listed explicitly in the list.")
 
     (tabbar-mode 1)
 
-(set-face-attribute
- 'tabbar-default nil
- :background (face-attribute 'default :background)
- :foreground (face-attribute 'default :background)
- :box `(:line-width 1 :color ,(face-attribute 'default :background)))
+    (set-face-attribute
+     'tabbar-default nil
+     :background (face-attribute 'fringe :background)
+     :foreground (face-attribute 'default :background)
+     :box `(:line-width 1 :color ,(face-attribute 'fringe :background)))
 
-(set-face-attribute
- 'tabbar-unselected nil
- :background (face-attribute 'default :background)
- :foreground (face-attribute 'default :foreground)
- :slant 'normal
- :weight 'medium
- :box `(:line-width 5 :color ,(face-attribute 'default :background)))
+    (set-face-attribute
+     'tabbar-unselected nil
+     :background (face-attribute 'fringe :background)
+     :foreground (face-attribute 'default :foreground)
+     :slant 'normal
+     :weight 'medium
+     :box `(:line-width 5 :color ,(face-attribute 'fringe :background)))
 
-(set-face-attribute
- 'tabbar-selected nil
- :background (face-attribute 'default :foreground)
- :foreground (face-attribute 'default :background)
- :box `(:line-width 5 :color ,(face-attribute 'default :foreground)))
+    (set-face-attribute
+     'tabbar-selected nil
+     :background (face-attribute 'default :foreground)
+     :foreground (face-attribute 'default :background)
+     :box `(:line-width 5 :color ,(face-attribute 'default :foreground)))
 
-(set-face-attribute
- 'tabbar-highlight nil
- :background (face-attribute 'default :foreground)
- :foreground (face-attribute 'default :background)
- :box `(:line-width 5 :color ,(face-attribute 'default :foreground)))
+    (set-face-attribute
+     'tabbar-highlight nil
+     :background (face-attribute 'default :foreground)
+     :foreground (face-attribute 'default :background)
+     :box `(:line-width 5 :color ,(face-attribute 'default :foreground)))
 
-(set-face-attribute
- 'tabbar-button nil
- :box `(:line-width 1 :color ,(face-attribute 'default :background)))
+    (set-face-attribute
+     'tabbar-button nil
+     :box `(:line-width 1 :color ,(face-attribute 'fringe :background)))
 
-(set-face-attribute
- 'tabbar-separator nil
- :background (face-attribute 'default :background)
- :height 0.6)
+    (set-face-attribute
+     'tabbar-separator nil
+     :background (face-attribute 'default :background)
+     :height 0.2)
 
-;; Change padding of the tabs
-;; we also need to set separator to avoid overlapping tabs by highlighted tabs
-(custom-set-variables
- '(tabbar-separator (quote (0.5))))
+    ;; Change padding of the tabs
+    ;; we also need to set separator to avoid overlapping tabs by highlighted tabs
+    (custom-set-variables
+     '(tabbar-separator (quote (0.5))))
 
-;; adding spaces
-(defun tabbar-buffer-tab-label (tab)
-  "Return a label for TAB.
+    ;; adding spaces
+    (defun tabbar-buffer-tab-label (tab)
+      "Return a label for TAB.
 That is, a string used to represent it on the tab bar."
-  (let ((label  (if tabbar--buffer-show-groups
-                    (format "[%s]  " (tabbar-tab-tabset tab))
-                  (format "%s  " (tabbar-tab-value tab)))))
-    ;; Unless the tab bar auto scrolls to keep the selected tab
-    ;; visible, shorten the tab label to keep as many tabs as possible
-    ;; in the visible area of the tab bar.
-    (if tabbar-auto-scroll-flag
-        label
-      (tabbar-shorten
-       label (max 1 (/ (window-width)
-                       (length (tabbar-view
-                                (tabbar-current-tabset)))))))))
+      (let ((label  (if tabbar--buffer-show-groups
+                        (format "[%s]  " (tabbar-tab-tabset tab))
+                      (format "%s  " (tabbar-tab-value tab)))))
+        ;; Unless the tab bar auto scrolls to keep the selected tab
+        ;; visible, shorten the tab label to keep as many tabs as possible
+        ;; in the visible area of the tab bar.
+        (if tabbar-auto-scroll-flag
+            label
+          (tabbar-shorten
+           label (max 1 (/ (window-width)
+                           (length (tabbar-view
+                                    (tabbar-current-tabset)))))))))
 
     )
-)
+  )
