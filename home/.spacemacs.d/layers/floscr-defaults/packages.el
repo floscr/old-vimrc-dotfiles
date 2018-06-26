94568114e1233bf4947158187c430c84c4960e62
Florian Schroedl <hello@florianschroedl.com>
2018-06-26 16:47:43 +0200

    Set CMD-w to kill either a window or buffer

--- home/.spacemacs.d/layers/floscr-defaults/packages.el
+++ home/.spacemacs.d/layers/floscr-defaults/packages.el
@@ -1,6 +1,11 @@
 (defconst floscr-defaults-packages
   '(neotree))
 
+(defun floscr-defaults/kill-window-or-buffer ()
+  (interactive)
+  (condition-case nil (delete-window) (error (kill-current-buffer)))
+  )
+
 (defun floscr-defaults/post-init-neotree ()
   (use-package neotree
     :config
