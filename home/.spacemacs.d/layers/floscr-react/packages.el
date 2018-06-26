8b359133cefa3e694b4b81884fbf6ace75368537
Florian Schroedl <hello@florianschroedl.com>
2018-06-26 17:33:57 +0200

    Update layer

--- home/.spacemacs.d/layers/floscr-react/packages.el
+++ home/.spacemacs.d/layers/floscr-react/packages.el
@@ -1,28 +1,21 @@
 (defconst floscr-react-packages
   '(
-    add-node-modules-path
     flycheck
     eslintd-fix
     evil-matchit
+    rjsx-mode
     ))
 
-(defun floscr-react/post-init-add-node-modules-path ()
-  (with-eval-after-load 'react-mode
-    (add-hook 'react-mode-hook #'add-node-modules-path)))
-
-(eval-after-load 'react-mode
-  '(add-hook 'react-mode-hook #'add-node-modules-path))
-
-(add-hook 'react-mode-hook 'eslintd-fix-mode)
+(add-hook 'rjsx-mode-hook 'eslintd-fix-mode)
 
 (defun floscr-react/post-init-flycheck ()
   (with-eval-after-load 'flycheck
     (push 'javascript-jshint flycheck-disabled-checkers)
     (push 'json-jsonlint flycheck-disabled-checkers))
-  (spacemacs/enable-flycheck 'react-mode))
+  (spacemacs/enable-flycheck 'rjsx-mode))
 
-(defun floscr-react/init-react-mode ()
-  (use-package react-mode
+(defun floscr-react/init-rjsx-mode ()
+  (use-package rjsx-mode
     :defer t
     :init
     (progn
