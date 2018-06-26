89d3e9b46e5f3d338ef55f00a1ea17c9d8a9be9c
Florian Schroedl <hello@florianschroedl.com>
2018-06-26 21:59:27 +0200

    Add tabbar layer

---
+++ home/.spacemacs.d/layers/tabbar/packages.el
@@ -0,0 +1,83 @@
+(defvar tabbar-packages
+  '(
+    tabbar
+    )
+  "List of all packages to install and/or initialize. Built-in packages
+which require an initialization must be listed explicitly in the list.")
+
+(face-attribute 'default :background)
+
+(defvar tabbar-excluded-packages '()
+  "List of packages to exclude.")
+
+;; For each package, define a function tabbar/init-<package-tabbar>
+;;
+;; (defun tabbar/init-my-package ()
+;;   "Initialize my package"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
+(defun tabbar/init-tabbar ()
+  "Initialize tabbar"
+  (use-package tabbar
+    :init
+
+    (tabbar-mode 1)
+
+(set-face-attribute
+ 'tabbar-default nil
+ :background (face-attribute 'default :background)
+ :foreground (face-attribute 'default :background)
+ :box '(:line-width 1 :color "gray20" :style nil))
+
+(set-face-attribute
+ 'tabbar-unselected nil
+ :background (face-attribute 'mode-line :background)
+ :foreground "white"
+ :box `(:line-width 5 :color ,(face-attribute 'mode-line :background)))
+
+ ;; :box '(:line-width 5 :color ,(face-attribute 'default :background) :style nil))
+(set-face-attribute
+ 'tabbar-selected nil
+ :background "gray75"
+ :foreground "black"
+ :box '(:line-width 5 :color "gray75" :style nil))
+(set-face-attribute
+ 'tabbar-highlight nil
+ :background "white"
+ :foreground "black"
+ :underline nil
+ :box '(:line-width 5 :color "white" :style nil))
+(set-face-attribute
+ 'tabbar-button nil
+ :box '(:line-width 1 :color "gray20" :style nil))
+(set-face-attribute
+ 'tabbar-separator nil
+ :background "gray20"
+ :height 0.6)
+
+;; Change padding of the tabs
+;; we also need to set separator to avoid overlapping tabs by highlighted tabs
+(custom-set-variables
+ '(tabbar-separator (quote (0.5))))
+;; adding spaces
+(defun tabbar-buffer-tab-label (tab)
+  "Return a label for TAB.
+That is, a string used to represent it on the tab bar."
+  (let ((label  (if tabbar--buffer-show-groups
+                    (format "[%s]  " (tabbar-tab-tabset tab))
+                  (format "%s  " (tabbar-tab-value tab)))))
+    ;; Unless the tab bar auto scrolls to keep the selected tab
+    ;; visible, shorten the tab label to keep as many tabs as possible
+    ;; in the visible area of the tab bar.
+    (if tabbar-auto-scroll-flag
+        label
+      (tabbar-shorten
+       label (max 1 (/ (window-width)
+                       (length (tabbar-view
+                                (tabbar-current-tabset)))))))))
+
+    )
+)
