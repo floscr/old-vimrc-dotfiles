61c4161604aef05ae7f75687f0a9f313807696dd
Florian Schroedl <hello@florianschroedl.com>
2018-06-26 17:28:23 +0200

    Add custom fork of rjsx mode to packages...

--- home/.spacemacs.d/init.el
+++ home/.spacemacs.d/init.el
@@ -62,6 +62,7 @@ This function should only modify configuration layer settings."
 
    dotspacemacs-additional-packages '(
                                       editorconfig
+                                      (rjsx-mode :location (recipe :fetcher github :repo "floscr/rjsx-mode"))
                                       evil-replace-with-register
                                       eslintd-fix
                                       helm-ls-git
