60dc5a9777c7c3e2a7c6cbcec73e02cb36e28d29
Florian Schroedl <hello@florianschroedl.com>
2018-07-09 20:32:25 +0200

    Add bindings file

---
+++ home/.config/doom/+bindings.el
@@ -0,0 +1,8 @@
+;;; ~/.homesick/repos/dotfiles/home/.config/doom/+bindings.el -*- lexical-binding: t; -*-
+
+(map!
+ :n "M-="   #'default-text-scale-increase
+ :n "M--"   #'default-text-scale-decrease
+ :n "M-0"   #'default-text-scale-reset
+ )
+
