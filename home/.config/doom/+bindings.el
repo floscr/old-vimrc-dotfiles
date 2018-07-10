4783aef360495998095d55f20629a47e6dcdf186
Florian Schroedl <hello@florianschroedl.com>
2018-07-10 09:01:31 +0200

    Allow resizing in all modes

--- home/.config/doom/+bindings.el
+++ home/.config/doom/+bindings.el
@@ -1,8 +1,8 @@
 ;;; ~/.homesick/repos/dotfiles/home/.config/doom/+bindings.el -*- lexical-binding: t; -*-
 
 (map!
- :n "M-="   #'default-text-scale-increase
- :n "M--"   #'default-text-scale-decrease
- :n "M-0"   #'default-text-scale-reset
+ :niv "M-="   #'default-text-scale-increase
+ :niv "M--"   #'default-text-scale-decrease
+ :niv "M-0"   #'default-text-scale-reset
  )
 
