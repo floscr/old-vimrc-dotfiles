da44be6a1cc8a4206af0a97e8f6b4be008b9b23c
Florian Schroedl <hello@florianschroedl.com>
2018-07-11 21:23:59 +0200

    Add umlaut bindings to insert mode

--- home/.config/doom/+bindings.el
+++ home/.config/doom/+bindings.el
@@ -5,6 +5,16 @@
  :niv "M--"   #'default-text-scale-decrease
  :niv "M-0"   #'default-text-scale-reset
 
+ ;; Umlaut
+ :i "A-;"   (λ! (insert "ö"))
+ :i "A-:"   (λ! (insert "Ö"))
+ :i "A-'"   (λ! (insert "ä"))
+ :i "A-\""  (λ! (insert "Ä"))
+ :i "A-["   (λ! (insert "ü"))
+ :i "A-{"   (λ! (insert "Ü"))
+ :i "A-s"   (λ! (insert "ß"))
+
+ :n "gb" #'evil-switch-to-windows-last-buffer
  :n "[1" #'+MM|other-file
  :n "]1" #'+MM|other-file
 
@@ -12,5 +22,3 @@
  :n "ss"   #'helm-swoop
  :n "au"   #'undo-tree-visualize
  )
-
-
