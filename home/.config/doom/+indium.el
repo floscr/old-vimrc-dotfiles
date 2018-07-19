e58a60d03caa6bcd812acfa39940f9836a049e99
Florian Schroedl <hello@florianschroedl.com>
2018-07-19 16:47:08 +0200

    Add inidium config file

---
+++ home/.config/doom/+indium.el
@@ -0,0 +1,5 @@
+;;; ~/.homesick/repos/Dotfiles/home/.config/doom/+indium.el -*- lexical-binding: t; -*-
+
+(def-package! indium
+  :config
+  (setq indium-chrome-executable "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"))
