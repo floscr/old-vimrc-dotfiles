b89229383ade196b632618fff8cd5f33b9ec896b
Florian Schroedl <hello@florianschroedl.com>
2018-07-19 20:32:58 +0200

    Remove backup files

--- home/.spacemacs.d.bak/gui-interactions.el
+++
@@ -1,7 +0,0 @@
-;; Fix the colors for osx
-(setq ns-use-srgb-colorspace nil)
-;; Store undos after closing emacs
-;; enabling to undo after closing a file
-(global-undo-tree-mode)
-(setq undo-tree-auto-save-history t)
-(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
