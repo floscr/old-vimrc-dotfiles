6772a798fead8476066bb3df34abb233c761dc8e
Florian Schroedl <hello@florianschroedl.com>
2018-08-10 15:03:44 +0200

    Add function to automatically generate index import files

--- home/.config/doom/+js.el
+++ home/.config/doom/+js.el
@@ -2,3 +2,30 @@
 
 (setq flycheck-javascript-eslint-executable (executable-find "eslint_d"))
 (after! rjsx-mode (add-hook 'js2-mode-hook #'eslintd-fix-mode))
+
+(defun remove-js-ext (f)
+  "Remove js extension from string"
+  (replace-regexp-in-string "\.js$" "" f))
+
+(defun buffer-file-name-relative ()
+  "Extranct the filename with extension from path"
+  (replace-regexp-in-string (file-name-directory buffer-file-name) "" (buffer-file-name)))
+
+(defun js-index-file-names ()
+  "Get filenames from current buffers directory"
+  (let ((fs (directory-files default-directory nil ".*\\.js")))
+    (mapcar 'remove-js-ext
+            (remove (buffer-file-name-relative) fs))
+    ))
+
+(defun +js|generate-index ()
+  "Generate an index import file for files in directory"
+  (interactive)
+  (erase-buffer)
+  (let* ((fs (js-index-file-names)))
+    (mapc (lambda (f) (insert "import " f " from './" f "';\n")) fs)
+    (insert "\n")
+    (insert "export default {\n")
+    (mapc (lambda (f) (insert "    " f ",\n")) fs)
+    (insert "};")
+    ))
