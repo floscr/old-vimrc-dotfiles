bd3309a938b1ad50c671824aa90380e9c95128b6
Florian Schroedl <hello@florianschroedl.com>
2018-08-13 15:34:40 +0200

    Add helper to open org file

--- home/.config/doom/+org.el
+++ home/.config/doom/+org.el
@@ -53,15 +53,20 @@
       '(("[…]" . "grey")
         ))
 
+(defun +org|find-file (f)
+  "Find file in org directory"
+  (find-file (concat org-directory f)))
+
 (defun +org|org-open-home-file ()
    "Open the home org file"
    (interactive)
-   (find-file (concat org-directory "/home.org")))
+    (+org|find-file "/home.org"))
+
 
 (defun +org|org-open-work-file ()
    "Open the home org file"
    (interactive)
-   (find-file (concat org-directory "/Work/work.org")))
+    (+org|find-file "/Work/work.org"))
 
 (defun +org|paste-chrome-link ()
   "Paste the frontmost chrome link"
