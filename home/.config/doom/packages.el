a6b89db3416963725c790394690c446d65f3e8d1
Florian Schroedl <hello@florianschroedl.com>
2018-08-21 17:55:39 +0200

    Add plur plugin

--- home/.config/doom/packages.el
+++ home/.config/doom/packages.el
@@ -10,3 +10,8 @@
 (package! evil-plugin :recipe (:fetcher github :repo "tarao/evil-plugins"))
 
 (package! evil-briefcase :recipe (:fetcher github :repo "strickinato/evil-briefcase"))
+
+;; Like abolish.vim
+;; Search and replace multiples
+;; TODO: Add literate config here
+(package! plur)
