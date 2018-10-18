47209ae7677800b010f7959f6243a58e8babfa7d
Florian Schroedl <hello@florianschroedl.com>
2018-10-18 19:01:44 +0200

    Add custom agenda for private listing

--- home/.config/doom/+org.el
+++ home/.config/doom/+org.el
@@ -162,6 +162,20 @@ E.g.: (Brackets signal the cursor position)
 (setq org-journal-time-prefix "* ")
 (setq org-journal-time-format "")
 
+
+(after! org-agenda
+  (setq org-agenda-custom-commands '())
+  (add-to-list 'org-agenda-custom-commands
+               '("p" "Private" agenda ""
+                 ((org-agenda-ndays 5)
+                  (org-agenda-span 7)
+                  (org-agenda-tag-filter-preset '("-WORK"))
+                  (org-agenda-tag-filter-preset '("-REPEATING"))
+                  (org-agenda-start-on-weekday 0)
+                  (org-agenda-time-grid nil)
+                  (org-agenda-start-on-weekday 1)
+                  (org-agenda-repeating-timestamp-show-all t)))))
+
 (after! org
   (map! :map evil-org-mode-map
 
