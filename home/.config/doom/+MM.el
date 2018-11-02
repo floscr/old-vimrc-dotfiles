a3fc9eb808addcc83363c98ad45d758bbddac883
Florian Schroedl <hello@florianschroedl.com>
2018-11-02 07:10:02 +0100

    Parinfer fixes

--- home/.config/doom/+MM.el
+++ home/.config/doom/+MM.el
@@ -4,8 +4,8 @@
 
 (after! yasnippet
   :config
-  (setq yas-snippet-dirs (append yas-snippet-dirs '("~/Code/Meisterlabs/Snippets")))
-  )
+  (setq yas-snippet-dirs (append yas-snippet-dirs '("~/Code/Meisterlabs/Snippets"))))
+
 
 (defun +MM|other-file ()
   "Toggle between component or controller"
@@ -13,15 +13,15 @@
   (setq filename (file-name-nondirectory buffer-file-name))
   (setq path (file-name-directory buffer-file-name))
   (setq target (if (string= filename "component.js") "controller.js" "component.js"))
-  (find-file (concat path target))
-  )
+  (find-file (concat path target)))
+
 
 (fset '+MM|turn-style-object-into-function
    (lambda (&optional arg)
      "Turns an object into a Style function, needs to be focused on the starting {"
      (interactive "p")
-     (kmacro-exec-ring-item (quote ([?y ?s ?a ?B ?b ?i ?S ?t ?y ?l ?e escape ?l ?a ?f ?u ?n ?c ?t ?i ?o ?n ?  S-backspace ?  ?\( ?o ?p ?t ?i ?o ?n ?s ?, ?  ?R ?u ?l ?e ?s escape ?l ?l ?y ?s ?a ?B ?B ?i ?  escape ?l ?a return ?r ?e ?t ?u ?r ?n ?  escape ?l ?j ?> ?i ?\{ ?k ?$ ?% ?a return escape ?k ?a ?\; escape ?= ?= ?j ?b ?l ?%] 0 "%d")) arg)
-     ))
+     (kmacro-exec-ring-item (quote ([?y ?s ?a ?B ?b ?i ?S ?t ?y ?l ?e escape ?l ?a ?f ?u ?n ?c ?t ?i ?o ?n ?  S-backspace ?  ?\( ?o ?p ?t ?i ?o ?n ?s ?, ?  ?R ?u ?l ?e ?s escape ?l ?l ?y ?s ?a ?B ?B ?i ?  escape ?l ?a return ?r ?e ?t ?u ?r ?n ?  escape ?l ?j ?> ?i ?\{ ?k ?$ ?% ?a return escape ?k ?a ?\; escape ?= ?= ?j ?b ?l ?%] 0 "%d")) arg)))
+
 
 (defun dated-string (name)
   (format "%s-name" (format-time-string "%m-%d")))
@@ -30,8 +30,8 @@
   "Insert tap log"
   (interactive)
   (newline-and-indent)
-  (yas-lookup-snippet "Tap Console Log" 'js2-mode)
-  )
+  (yas-lookup-snippet "Tap Console Log" 'js2-mode))
+
 
 ;; (defun +MM|toggle-relative()
 ;;   (interactive)
