508d450607f805dd559dd1ae60589846cda55eea
Florian Schroedl <hello@florianschroedl.com>
2018-11-07 22:30:58 +0100

    Remove duplicate savehist-mode

--- home/.config/doom/+git.el
+++ home/.config/doom/+git.el
@@ -58,8 +58,7 @@ If possible also go to the pointing line"
    magithub-clone-default-directory "~/Code/Repositories"
    git-commit-summary-max-length 120)
   (bind-key "M-r" #'+git|commit-search-message-history git-commit-mode-map)
-  (add-to-list 'savehist-additional-variables log-edit-comment-ring)
-  (savehist-mode +1))
+  (add-to-list 'savehist-additional-variables log-edit-comment-ring))
 
 (map!
  :leader
