0e58c3495d91d5709e048e68d561fe46f0eedd97
Florian Schroedl <hello@florianschroedl.com>
2018-11-08 17:13:35 +0100

    Remove curry, rcurry and compose functions

--- home/.config/doom/+utils.el
+++ home/.config/doom/+utils.el
@@ -40,45 +40,6 @@
   (interactive)
   (math-on-number '*))
 
-(defsubst curry (function &rest arguments)
-  (lexical-let ((function function)
-                (arguments arguments))
-    (lambda (&rest more) (apply function (append arguments more)))))
-
-(defsubst rcurry (function &rest arguments)
-  (lexical-let ((function function)
-                (arguments arguments))
-    (lambda (&rest more) (apply function (append more arguments)))))
-
-(defsubst compose (function &rest more-functions)
-  (cl-reduce (lambda (f g)
-               (lexical-let ((f f) (g g))
-                 (lambda (&rest arguments)
-                   (funcall f (apply g arguments)))))
-             more-functions
-             :initial-value function))
-
-;;; compact display
-(defun pretty-curry-compose ()
-  (mapc (lambda (pair)
-          (let ((regexp (car pair))
-                (symbol (cdr pair)))
-            (font-lock-add-keywords 'emacs-lisp-mode
-              `((,regexp
-                 (0 (progn (compose-region (match-beginning 1) (match-end 1)
-                                           ,symbol)
-                           nil)))))))
-        '(("(\\(compose\\)[ \t\n\r]" . ?\∘)
-          ("(\\(curry\\)[ \t\n\r]" . ?\»)
-          ("(\\(rcurry\\)[ \t\n\r]" . ?\«))))
-(add-to-list 'emacs-lisp-mode-hook 'pretty-curry-compose)
-
-;;; color these functions like keywords
-(font-lock-add-keywords 'emacs-lisp-mode
-                        '(("(\\(compose\\)[ \t\n\r]" 1 font-lock-keyword-face)
-                          ("(\\(curry\\)[ \t\n\r]" 1 font-lock-keyword-face)
-                          ("(\\(rcurry\\)[ \t\n\r]" 1 font-lock-keyword-face)))
-
 (defun delete-current-buffer-file ()
   "Removes file connected to current buffer and kills buffer."
   (interactive)
