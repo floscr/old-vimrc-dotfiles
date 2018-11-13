540c88f8fce6f2744033eafe3dd84f823986f25d
Florian Schroedl <hello@florianschroedl.com>
2018-11-13 17:30:56 +0100

    Add +org-web-tools-dwim-at-point

--- home/.config/doom/+utils.el
+++ home/.config/doom/+utils.el
@@ -1,5 +1,20 @@
 ;;; ~/.homesick/repos/Dotfiles/home/.config/doom/+utils.el -*- lexical-binding: t; -*-
 
+(defun +org-web-tools-dwim-at-point ()
+  "Pass url to web tools from either:
+1. An org link under the cursor
+2. An url in the clipboard"
+  (interactive)
+  (let ((org-url (org-element-property :raw-link (org-element-context)))
+        (clipboard-url (current-kill 0)))
+    (if org-url
+        (message "Reading org url from thing at point")
+        (org-web-tools-read-url-as-org org-url)
+      (if (string-match url-handler-regexp clipboard-url)
+          (message "Reading org url from clipboard")
+          (org-web-tools-read-url-as-org clipboard-url)
+        (message "No url found")))))
+
 (defun copy-message (x)
   (kill-new x)
   (message "Copied to clipboard: %s" x))
