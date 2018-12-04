304e9d8e090f9d11fc39347835c933e0754a7d5d
Florian Schroedl <hello@florianschroedl.com>
2018-12-04 10:33:07 +0100

    Remove books file from agenda

--- home/.config/doom/+org.el
+++ home/.config/doom/+org.el
@@ -231,7 +231,7 @@ E.g.: (Brackets signal the cursor position)
 
   (setq level-1-refile-targets (expand-org-file-names '("reading-list.org"
                                                         "cooking.org"
-                                                        "books.org"
+                                                        ;; "books.org"
                                                         "programming.org"
                                                         "shoppinglist.org")))
 
