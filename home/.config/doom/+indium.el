d8e15155bd9cc7500dc14981b2086c3105a6cfd7
Florian Schroedl <hello@florianschroedl.com>
2018-12-18 20:14:30 +0100

    Remove old org files

--- home/.config/doom/+indium.el
+++
@@ -1,5 +0,0 @@
-;;; ~/.homesick/repos/Dotfiles/home/.config/doom/+indium.el -*- lexical-binding: t; -*-
-
-(def-package! indium
-  :config
-  (setq indium-chrome-executable "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"))
