99173d9abe3bad7473dba2167d444535e159f7ff
Florian Schroedl <hello@florianschroedl.com>
2019-06-07 20:00:20 +0200

    Move to org-web-tools

--- home/.config/doom/modules/private/org/+org-web-tools.el
+++ home/.config/doom/modules/private/org/+org-web-tools.el
@@ -1,5 +1,25 @@
 ;;; lang/+org/+org-web-tools.el -*- lexical-binding: t; -*-
 
+(defun +org-web-tools/dwim-at-point ()
+  "Pass url to web tools from either:
+1. An org link under the cursor
+2. An url in the clipboard"
+  (interactive)
+  (let ((org-url (org-element-property :raw-link (org-element-context)))
+        (clipboard-url (current-kill 0)))
+    (if org-url
+        (message "Reading org url from thing at point")
+      (org-web-tools-read-url-as-org org-url)
+      (if (string-match url-handler-regexp clipboard-url)
+          (message "Reading org url from clipboard")
+        (org-web-tools-read-url-as-org clipboard-url)
+        (message "No url found")))))
+
+(defun +org-web-tools/read-url-at-point ()
+  "Open the url under the cursor"
+  (interactive)
+  (org-web-tools-read-url-as-org (org-web-tools--read-url)))
+
 (def-package! org-web-tools
   :after org
-  :command (+org-web-tools/read-url-at-point))
+  :commands (+org-web-tools/read-url-at-point))
