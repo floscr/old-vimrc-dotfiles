4b7570691bcd1742edf2e54e249d36e6e9ed2865
Florian Schroedl <hello@florianschroedl.com>
2019-09-09 19:33:45 +0200

    Add demo user to the trustedUsers

--- configuration.virtualbox.nix
+++ configuration.virtualbox.nix
@@ -19,6 +19,8 @@
     ./modules/shell/zsh.nix
   ];
 
+  nix.trustedUsers = [ "demo" ];
+
   services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
   services.xserver.displayManager.sddm.enable = lib.mkForce false;
 }
