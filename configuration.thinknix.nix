8438d3b63f1e09470464ac062999c5172fd1fac5
Florian Schroedl <hello@florianschroedl.com>
2019-09-17 17:06:22 +0200

    Disable

--- configuration.thinknix.nix
+++ configuration.thinknix.nix
@@ -44,14 +44,14 @@ in {
   hardware.pulseaudio.enable = true;
   #
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.autorun = true;
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.autorun = true;
 
   # Enable touchpad support.
   services.xserver.libinput.enable = true;
 
   # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
 }
