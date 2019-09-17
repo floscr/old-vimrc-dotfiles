9af47cb493f5674080ad040d609ba72ba2cbcda7
Florian Schroedl <hello@florianschroedl.com>
2019-09-17 16:16:06 +0200

    Config

--- configuration.shiro.nix
+++ configuration.shiro.nix
@@ -1,5 +1,3 @@
-# Shiro -- my laptop
-
 { config, lib, pkgs, ... }:
 
 let nixos-hardware = builtins.fetchTarball https://github.com/NixOS/nixos-hardware/archive/master.tar.gz;
@@ -8,43 +6,30 @@ in {
     ./.  # import common settings
 
     # Hardware specific
-    "${nixos-hardware}/dell/xps/13-9370"
+    "${nixos-hardware}/lenovo/thinkpad/t490"
 
     ./modules/desktop/bspwm.nix
 
     ./modules/browser/firefox.nix
+    ./modules/editors/emacs.nix
     ./modules/dev
-    ./modules/graphics/aseprite.nix
-    ./modules/graphics/gimp.nix
     ./modules/shell/direnv.nix
     ./modules/shell/git.nix
     ./modules/shell/gnupg.nix
-    ./modules/shell/pass.nix
     ./modules/shell/zsh.nix
-    # ./modules/video/obs.nix
 
     # Themes
-    ./themes/pianocat
+    ./themes/glimpse
   ];
 
-  networking.hostName = "shiro";
+  networking.hostName = "thinknix";
   networking.wireless.enable = true;
 
   # Optimize power use
-  environment.systemPackages = [ pkgs.powertop pkgs.acpi ];
+  environment.systemPackages = [ pkgs.powertop ];
   services.tlp.enable = true;
   powerManagement.powertop.enable = true;
 
   # Monitor backlight control
   programs.light.enable = true;
-
-  # Encrypted /home
-  boot.initrd.luks.devices = [
-    {
-      name = "home";
-      device = "/dev/nvme0n1p8";
-      preLVM = true;
-      allowDiscards = true;
-    }
-  ];
 }
