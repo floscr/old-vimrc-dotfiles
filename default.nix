f106844b97c0c0cdc8728a8833c4b83d3d82284e
Florian Schroedl <hello@florianschroedl.com>
2019-09-18 18:11:54 +0200

    Add home-manager

--- default.nix
+++ default.nix
@@ -63,6 +63,10 @@
     shell = pkgs.zsh;
   };
 
+  home-manager.users.floscr = {
+    programs.zsh.enable = true;
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
