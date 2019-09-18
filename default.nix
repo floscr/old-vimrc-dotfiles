dc83032e6b8d86e5e4092d170da2a5ccabd9a92a
Florian Schroedl <hello@florianschroedl.com>
2019-09-18 10:47:16 +0200

    set boot loader

--- default.nix
+++ default.nix
@@ -3,6 +3,10 @@
     # Nothing in /tmp should survive a reboot
     boot.cleanTmpDir = true;
 
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
     boot.initrd.luks.devices = [{
       name = "root";
       device = "/dev/nvme0n1p2";
