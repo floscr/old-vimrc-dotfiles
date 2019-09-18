dcb45758f5a004009a0366ffa4231e814baa7103
Florian Schroedl <hello@florianschroedl.com>
2019-09-18 18:01:02 +0200

    remove lib

--- default.nix
+++ default.nix
@@ -1,65 +1,66 @@
-{ config, lib, pkgs, ...}: with lib;
-  {
-    # Nothing in /tmp should survive a reboot
-    boot.cleanTmpDir = true;
+{ config, pkgs, ...}:
 
-    # Use the systemd-boot EFI boot loader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+{
+  # Nothing in /tmp should survive a reboot
+  boot.cleanTmpDir = true;
 
-    nix = {
-      autoOptimiseStore = true;
-      trustedUsers = [ "root" "@wheel" ];
-    };
-    nixpkgs.config.allowUnfree = true;
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    # Cpu throttling
-    services.thermald.enable = true;
+  nix = {
+    autoOptimiseStore = true;
+    trustedUsers = [ "root" "@wheel" ];
+  };
+  nixpkgs.config.allowUnfree = true;
 
-    environment.systemPackages = with pkgs; [
-      bc
-      coreutils
-      tree
-      firefox
-      git
-      htop
-      killall
-      networkmanager
-      networkmanagerapplet
-      rofi
-      unzip
-      vim
-      wget
-    ];
+  # Cpu throttling
+  services.thermald.enable = true;
 
-    # Enable sound.
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
+  environment.systemPackages = with pkgs; [
+    bc
+    coreutils
+    tree
+    firefox
+    git
+    htop
+    killall
+    networkmanager
+    networkmanagerapplet
+    rofi
+    unzip
+    vim
+    wget
+  ];
 
-    # Enable the X11 windowing system.
-    services.xserver.enable = true;
-    services.xserver.layout = "us";
-    # services.xserver.xkbOptions = "eurosign:e";
-    services.xserver.windowManager.i3.enable = true;
-    services.xserver.autorun = true;
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
-    # Enable touchpad support.
-    services.xserver.libinput.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.autorun = true;
 
-    # Enable the KDE Desktop Environment.
-    services.xserver.displayManager.sddm.enable = true;
-    services.xserver.desktopManager.plasma5.enable = true;
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
 
-    users.users.floscr = {
-      isNormalUser = true;
-      uid = 1000;
-      extraGroups = [ "wheel" "video" "networkmanager" ];
-      shell = pkgs.zsh;
-    };
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
-    # This value determines the NixOS release with which your system is to be
-    # compatible, in order to avoid breaking some software such as database
-    # servers. You should change this only after NixOS release notes say you
-    # should.
-    system.stateVersion = "19.03"; # Did you read the comment?
-  }
+  users.users.floscr = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" "video" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
+}
