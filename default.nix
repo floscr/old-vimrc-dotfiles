e420685d0fc280bc454d220f7eff21f2614553b9
Florian Schroedl <hello@florianschroedl.com>
2019-09-20 16:13:31 +0000

    Fix rebuild binding

--- default.nix
+++ default.nix
@@ -56,8 +56,8 @@
       nix-env = "NIXPKGS_ALLOW_UNFREE=1 nix-env";
       ne = "nix-env";
       nu = "sudo nix-channel --update && sudo nixos-rebuild -I config=$HOME/.dotfiles/config switch";
-      nre = "sudo nixos-rebuild -I config=$HOME/.dotfiles/config";
       ngc = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
+      nre = "sudo nixos-rebuild switch -I config=/etc/dotfiles/config";
       sudo = "sudo ";
     };
   };
