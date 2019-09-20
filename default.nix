76842d3a93ab6c74eb03f261be15e3a74db49c4e
Florian Schroedl <hello@florianschroedl.com>
2019-09-20 13:16:15 +0200

    Add aliases

--- default.nix
+++ default.nix
@@ -52,6 +52,11 @@
     shellAliases = {
       q = "exit";
       clr = "clear";
+      nix-env = "NIXPKGS_ALLOW_UNFREE=1 nix-env";
+      ne = "nix-env";
+      nu = "sudo nix-channel --update && sudo nixos-rebuild -I config=$HOME/.dotfiles/config switch";
+      nre = "sudo nixos-rebuild -I config=$HOME/.dotfiles/config";
+      ngc = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
       sudo = "sudo ";
     };
   };
