ab1e441331fb2966ddf4761e02ed533d1a7b5c6e
Florian Schroedl <hello@florianschroedl.com>
2019-09-23 08:45:15 +0200

    Dropbox layer

--- modules/services/dropbox.nix
+++ modules/services/dropbox.nix
@@ -1,38 +1,15 @@
-self: super: {
+{ pkgs, ... }:
 
-  dropbox-filesystem-fix = with super; stdenv.mkDerivation rec {
-    name = "dropbox-filesystem-fix-${version}";
-    version = "20190417";
-
-    src = fetchFromGitHub {
-        owner = "dark";
-        repo = "dropbox-filesystem-fix";
-        rev = "72f4d04852d5002d9ba29b3f77fbacb2c12d1432";
-        sha256 = "03h95lncbw8mss1f67697jl3diiw7iyq6cxqlzgl3xwgxwbhqd0j";
-    };
-
-    installPhase = ''
-        mkdir -p $out/lib
-        cp libdropbox_fs_fix.so $out/lib
-    '';
-
-    meta = with stdenv.lib; {
-        description = "Fix filesystem detection in Dropbox";
-        license = licenses.gpl30;
-        platforms = platforms.linux;
-    };
-  };
-
-  dropbox-filesystem-agnostic = with super; stdenv.mkDerivation rec {
-    name = "dropbox-fixed";
-
-    buildInputs = [ makeWrapper ];
-
-    unpackPhase = "true";
-    installPhase = ''
-      mkdir -p $out/bin
-      makeWrapper ${pkgs.dropbox}/bin/dropbox $out/bin/dropbox \
-        --prefix LD_PRELOAD : ${self.dropbox-filesystem-fix}/lib/libdropbox_fs_fix.so
-    '';
-  };
+{
+  xdg.configFile."autostart/dropbox.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Exec=${pkgs.dropbox-filesystem-agnostic}/bin/dropbox
+    Terminal=false
+    Name=Dropbox
+    Categories=Network;FileTransfer;
+    Comment=Sync your files across computers and to the web
+    GenericName=File Synchronizer
+    StartupNotify=false
+  '';
 }
