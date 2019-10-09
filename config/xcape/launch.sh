f221565940cae40bd29062d9a465c08bc4a95941
Florian Schroedl <hello@florianschroedl.com>
2019-10-09 11:52:00 +0200

    Config launch.sh

--- config/xcape/launch.sh
+++ config/xcape/launch.sh
@@ -2,22 +2,21 @@
 
 pkill -9 -f xcape
 
-# xset r rate 180 120
+xset r rate 180 120
 
-# xmodmap -e "clear lock"
-# xmodmap -e "keycode 66 = Super_L"
-# xmodmap -e "keycode 36 = Super_R"
+xmodmap -e "clear lock"
+xmodmap -e "keycode 66 = Super_L"
+xmodmap -e "keycode 36 = Super_R"
 
+spare_modifier="Hyper_L"
+xmodmap -e "keycode 65 = $spare_modifier"
+xmodmap -e "remove mod4 = $spare_modifier"
+xmodmap -e "add Control = $spare_modifier"
 
-# spare_modifier="Hyper_L"
-# xmodmap -e "keycode 65 = $spare_modifier"
-# xmodmap -e "remove mod4 = $spare_modifier"
-# xmodmap -e "add Control = $spare_modifier"
+xmodmap -e "keycode any = space"
+xmodmap -e "keycode any = Return"
 
-# xmodmap -e "keycode any = space"
-# xmodmap -e "keycode any = Return"
-
-# xcape -t 1000 -e "Shift_L=Escape;Shift_R=Escape;Super_R=Return;$space_modifier=space"
+xcape -t 1000 -e "Shift_L=Escape;Shift_R=Escape;Super_R=Return;$space_modifier=space"
 
 setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
