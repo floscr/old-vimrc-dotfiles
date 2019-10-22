a36c4a15587e058a7f07cbaca7eb58a3f52144cf
Florian Schroedl <hello@florianschroedl.com>
2019-10-22 19:07:36 +0200

    Map spacebar to control

--- default.nix
+++ default.nix
@@ -99,9 +99,18 @@
 # disable Display Power Managing Signaling
 xset -dpms
 
-# Capslock to control
-setxkbmap -option ctrl:nocaps
-xcape -e 'Control_L=Escape'
+# Make space Control L whenn pressed.
+spare_modifier="Hyper_L"
+xmodmap -e "keycode 65 = $spare_modifier"
+xmodmap -e "remove mod4 = $spare_modifier"
+xmodmap -e "add Control = $spare_modifier"
+
+# Map space to an unused keycode (to keep it around for xcape to
+# use).
+xmodmap -e "keycode any = space"
+
+# Finally use xcape to cause the space bar to generate a space when tapped.
+xcape -e "$spare_modifier=space"
 
 sh ~/.config/polybar/launch.sh
     '';
