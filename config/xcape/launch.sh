61e2298ccfe027f0b9197232a540ebed24a41e3b
Florian Schroedl <hello@florianschroedl.com>
2019-10-30 16:40:13 +0100

    Adapt launch script

--- config/xcape/launch.sh
+++ config/xcape/launch.sh
@@ -4,19 +4,19 @@ pkill -9 -f xcape
 
 xset r rate 180 120
 
-xmodmap -e "clear lock"
-xmodmap -e "keycode 66 = Super_L"
-xmodmap -e "keycode 36 = Super_R"
-
+# Make space Control L whenn pressed.
 spare_modifier="Hyper_L"
 xmodmap -e "keycode 65 = $spare_modifier"
 xmodmap -e "remove mod4 = $spare_modifier"
 xmodmap -e "add Control = $spare_modifier"
 
+# Map space to an unused keycode (to keep it around for xcape to
+# use).
 xmodmap -e "keycode any = space"
-xmodmap -e "keycode any = Return"
 
-xcape -t 1000 -e "Shift_L=Escape;Shift_R=Escape;Super_R=Return;$space_modifier=space"
+# Finally use xcape to cause the space bar to generate a space when tapped.
+xcape -e "$spare_modifier=space"
 
+# Capslock to control
 setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
