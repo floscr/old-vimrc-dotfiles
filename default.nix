cd64e4fcc36a855acd283ff026533e1aa59a82f0
Florian Schroedl <hello@florianschroedl.com>
2019-10-31 17:21:51 +0100

    Move keyboard setup into script

--- default.nix
+++ default.nix
@@ -102,26 +102,10 @@
 # disable Display Power Managing Signaling
 xset -dpms
 
-# Make space Control L whenn pressed.
-spare_modifier="Hyper_L"
-xmodmap -e "keycode 65 = $spare_modifier"
-xmodmap -e "remove mod4 = $spare_modifier"
-xmodmap -e "add Control = $spare_modifier"
+greenclip daemon&
 
-# Map space to an unused keycode (to keep it around for xcape to
-# use).
-xmodmap -e "keycode any = space"
-
-# Finally use xcape to cause the space bar to generate a space when tapped.
-xcape -e "$spare_modifier=space"
-
-greenclip daemon &
-
-# Capslock to control
-setxkbmap -option ctrl:nocaps
-xcape -e 'Control_L=Escape'
-
-sh ~/.config/polybar/launch.sh
+sh ~/.config/polybar/launch.sh&
+sh ~/bin/setup-keyboard
     '';
   };
 
