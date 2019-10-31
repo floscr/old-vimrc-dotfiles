b50ee986852047885ef2b319707fe0f1eaadd9c1
Florian Schroedl <hello@florianschroedl.com>
2019-10-31 18:05:33 +0100

    Reduce chrome size

--- overlays/chromium.nix
+++ overlays/chromium.nix
@@ -2,6 +2,6 @@ self: super:
 
 {
   chromium = super.chromium.override {
-    commandLineArgs = ''--add-flags "--force-device-scale-factor=1.5"'';
+    commandLineArgs = ''--add-flags "--force-device-scale-factor=1.1"'';
   };
 }
