65084a5faba512b3d3c01d705ec4425e59b08b28
Florian Schroedl <flo.schroedl@gmail.com>
2019-11-12 10:10:03 +0100

    Add hidpi fix for spotify

--- modules/desktop/sound.nix
+++ modules/desktop/sound.nix
@@ -1,6 +1,20 @@
 #Enables bluetooth headset using bluez5 and pulseaudio
 { pkgs, ... }:
 
+let
+  # Spotify is terrible on hidpi screens (retina, 4k); this small wrapper
+  # passes a command-line flag to force better scaling.
+  spotify-4k = pkgs.symlinkJoin {
+    name = "spotify";
+    paths = [ pkgs.spotify ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/spotify \
+        --add-flags "--force-device-scale-factor=1.5"
+    '';
+  };
+
+in
 {
   #sudo rfkill unblock bluetooth
   hardware.bluetooth = {
@@ -41,6 +55,6 @@
     pkgs.playerctl
     pkgs.pavucontrol
     pkgs.blueman
-    pkgs.spotify
+    spotify-4k
   ];
 }
